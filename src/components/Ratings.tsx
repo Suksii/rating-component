04cd357816e809809c93ce0c15bbdd7892ba5a27
unknown <ramovic225@gmail.com>
2024-11-14 18:51:55 +0100

    functionality added

--- src/components/Ratings.tsx
+++ src/components/Ratings.tsx
@@ -1,11 +1,39 @@
+import { CSSProperties, useEffect, useState } from "react";
 import StarComponent from "./StarComponent";
 
 const Ratings = () => {
+  const [ratings, setRatings] = useState<number>(0);
+  const [colorStarIndex, setColorStarIndex] = useState<number | null>(null);
+
+  const handleRatings = (index: number) => {
+    setRatings(index + 1);
+  };
+
+  useEffect(() => {
+    console.log(ratings);
+  }, [ratings]);
   return (
-    <div className="stars_container">
-      {[...Array(10)].map((_, index) => (
-        <StarComponent key={index} />
-      ))}
+    <div style={{ display: "flex", gap: "4px" }}>
+      {[...Array(10)].map((_, index) => {
+        const isStarFilled =
+          colorStarIndex !== null ? colorStarIndex >= index : ratings > index;
+
+        return (
+          <div
+            key={index}
+            onClick={() => handleRatings(index)}
+            onMouseEnter={() => setColorStarIndex(index)}
+            onMouseLeave={() => setColorStarIndex(null)}
+            style={
+              {
+                color: isStarFilled ? "yellow" : "grey",
+              } as CSSProperties
+            }
+          >
+            <StarComponent />
+          </div>
+        );
+      })}
     </div>
   );
 };
--- src/components/StarComponent.tsx
+++ src/components/StarComponent.tsx
@@ -1,17 +1,9 @@
-import { CSSProperties, useState } from "react";
 import { FaStar } from "react-icons/fa";
 
 const StarComponent = () => {
-  const [colorStar, setColorStar] = useState<boolean>(false);
-
   return (
     <div className="single_star">
-      <FaStar
-        size={50}
-        onMouseEnter={() => setColorStar(true)}
-        onMouseLeave={() => setColorStar(false)}
-        style={{ color: colorStar ? "yellow" : "grey" } as CSSProperties}
-      />
+      <FaStar size={50} />
     </div>
   );
 };
